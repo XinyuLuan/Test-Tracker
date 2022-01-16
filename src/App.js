300f2be24db9f552ad930868a77d56962665131d
XinyuLuan <xinyujie0320@gmail.com>
2022-01-16 03:54:34 -0800

    List all task and add task form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,66 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
-  const name = "Brad";
-  const x = true;
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    // console.log("delete", id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+    // tasks.map((task) => {
+    //   if(task.id === id){
+    //     task.reminder = !task.reminder
+    //   }
+    // })
+
+    // setTesks([...tasks]);
+  };
+
   return (
     // Have to return single element
     <div className="container">
       {/* <h1>Hello word {name}</h1>
       <h2>Hello {x ? "yes" : "No"}</h2> */}
-      <Header title="name" />
+      <AddTask />
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
 // function doesn't need this
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 //prep to {title} distructure
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Click");
+  };
   return (
-    <header>
+    <header className="header">
       {/* <h1 style={headingStyle}>{title}</h1> */}
       <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick}></Button>
+      {/* <Button color="blue" text="Hello"></Button>
+      <Button color="red" text="Hello"></Button> */}
     </header>
   );
 };
